0fa0ff5df7586695a375a91ad668b1b3b2655dd6
Kuiliang Zhang <z1219202167@outlook.com>
2025-08-05 17:12:57 -0700

    feat: add sponsorkit

---
+++ sponsorkit.config.js
@@ -0,0 +1,42 @@
+import { defineConfig, tierPresets } from "sponsorkit";
+
+export default defineConfig({
+  // Providers configs
+  github: {
+    login: "mengxi-ream",
+    type: "user",
+  },
+
+  // Rendering configs
+  width: 800,
+  renderer: "tiers", // or 'circles'
+  formats: ["svg", "png"],
+  tiers: [
+    // Past sponsors, currently only supports GitHub
+    {
+      title: "Past Sponsors",
+      monthlyDollars: -1,
+      preset: tierPresets.xs,
+    },
+    // Default tier
+    {
+      title: "Backers",
+      preset: tierPresets.base,
+    },
+    {
+      title: "Sponsors",
+      monthlyDollars: 10,
+      preset: tierPresets.medium,
+    },
+    {
+      title: "Silver Sponsors",
+      monthlyDollars: 50,
+      preset: tierPresets.large,
+    },
+    {
+      title: "Gold Sponsors",
+      monthlyDollars: 100,
+      preset: tierPresets.xl,
+    },
+  ],
+});
